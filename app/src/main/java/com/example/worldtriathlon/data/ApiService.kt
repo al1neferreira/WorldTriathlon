02893b012d72d1ac4509626189a4ace2740d3ab4
Aline Ferreira <alinfst@gmail.com>
2024-07-01 19:55:57 -0300

    setting the parameter elite=true

--- app/src/main/java/com/example/worldtriathlon/data/ApiService.kt
+++ app/src/main/java/com/example/worldtriathlon/data/ApiService.kt
@@ -2,8 +2,10 @@ package com.example.worldtriathlon.data
 
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface ApiService {
     @GET("athletes?per_page=10")
-    fun getAthleteListings(): Call<AthleteResponse>
+    fun getAthleteListings(@Query("elite") elite: Boolean = true
+    ): Call<AthleteResponse>
 }
